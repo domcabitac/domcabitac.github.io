c565a3152a80b6f9669b2a0ec25aaf0cfefefcaf
Dominic Cabitac <dominic.cabitac@uoit.net>
2020-01-21 12:14:46 -0500

    added projects folder and light theme

--- scripts/index.js
+++ scripts/index.js
@@ -52,6 +52,6 @@ window.onload = function() {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ffffff}";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000000}";
     document.body.appendChild(css);
 };
